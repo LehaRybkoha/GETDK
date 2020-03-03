cd169babd593ee3e17f95cf411d99836c3c9be8a
Aleksei <super.rybko@mail.ru>
2020-03-03 23:44:27 +0300

    Second step done

--- src/blocks/modules/form/form.js
+++ src/blocks/modules/form/form.js
@@ -1,13 +1,3 @@
-modules.define('form', ['i-bem-dom'], function(provide, bemDom) {
-
-provide(bemDom.declBlock(this.name, {
-    onSetMod: {
-        js: {
-            inited: function() {
-                
-            }
-        }
-    }
-}));
-
-});
+import 'jquery.scrollto';
+import 'jquery-validation';
+import 'jquery-mask-plugin';
